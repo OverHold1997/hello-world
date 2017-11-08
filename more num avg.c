21ddb7e51392ac9e420ca3d0e868210682dc2970
OverHold1997 <33322407+OverHold1997@users.noreply.github.com>
2017-11-08 14:06:18 +0800

    Create more num avg.c

---
+++ more num avg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	int avg;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	scanf("%d",&d);
+	scanf("%d",&e);
+	scanf("%d",&f);
+	avg=(a+b+c+d+e+f)/6;
+	printf("avg=%d\n",avg);
+	return 0;
+}
