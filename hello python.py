2ff061a0a9aae4cdc00191c4e1524b4d889991cd
OverHold1997 <33322407+OverHold1997@users.noreply.github.com>
2018-08-03 15:17:02 +0800

    Add files via upload

---
+++ hello python.py
@@ -0,0 +1,18 @@
+"""
+变量赋值！在这里，我们创建了一个名为SPAMYLUTION的变量，
+并使用＝，Python的赋值运算符赋值为0。
+旁白：如果你已经用某些其他语言编程（比如Java或C++），
+你可能会注意到Python不需要我们在这里做的一些事情：
+在分配给它之前，我们不需要“声明”SPAMULY量。
+我们不需要告诉Python将引用什么类型的SPAMULY值。
+事实上，我们甚至可以重新分配SPAMION量来引用不同类型的东西，比如字符串或布尔值。
+"""
+spam_amount=0
+print(spam_amount)
+
+spam_amount=spam_amount+4
+if spam_amount>0:
+    print("But I dont't wany ANY spam")
+
+    viking_song ="Spam"*spam_amount
+    print(viking_song)
