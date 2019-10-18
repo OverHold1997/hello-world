e8fae97aeb85d3ccd034f5300bba2e9ab99e0351
OverHold1997 <33322407+OverHold1997@users.noreply.github.com>
2019-10-18 12:36:27 +0000

    Delete filel.cpp

--- arraymax/filel.cpp
+++
@@ -1,9 +0,0 @@
-#include "arraymax.h"
-int main()
-{
-	Array_max arrmax;//定义对象arrmax 
-	arrmax.set_value();//调用arrmax的set_value函数，向数组元素输入数值 
-	arrmax.max_value();//调用arrmax的max_value函数，找出数组元素中的最大值 
-	arrmax.show_value();//调用arrmax的show_value函数，输出数组元素中的最大值 
-	return 0;
-}
