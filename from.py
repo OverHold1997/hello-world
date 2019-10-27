8aff046d39209f701714ec59eefca7830b1c1178
OverHold1997 <33322407+OverHold1997@users.noreply.github.com>
2019-10-27 15:46:32 +0000

    Delete from.py

--- from.py
+++
@@ -1,5 +0,0 @@
-print("You've successfully run some Python code")
-print("Congratulations!")
-from learntools.core import binder; binder.bind(globals())
-from learntools.python.ex1 import *
-print("Setup complete! You're ready to start question 0.")
